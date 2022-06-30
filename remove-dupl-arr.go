a2a72318a83c3ea150fd45a67e91c6418fad66a9
Maxim Yefremov <maxYefr@gmail.com>
2022-06-30 11:49:05 +0300

    all pass

--- remove-dupl-arr.go
+++ remove-dupl-arr.go
@@ -1,6 +1,10 @@
 package leetcode
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
 // https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
 func RemoveDuplicates(nums []int) int {
@@ -8,24 +12,72 @@ func RemoveDuplicates(nums []int) int {
 		toDeleteCount     int
 		totalDeletedCount int
 	)
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] == nums[i+1] {
-			totalDeletedCount++
+	var latestSameValue int
+	i := -1
+	lastGoodIndex := 0
+	for {
+		i++
+		log.Println("i", i)
+		log.Println("toDeleteCount", toDeleteCount)
+		//time.Sleep(time.Millisecond * 10)
+		if i >= len(nums)-1 {
+			log.Printf("greater or equal than nums len: %+v", nums)
+			break
+		}
+		if i < len(nums)-1 && nums[i] == nums[i+1] {
 			toDeleteCount++
+			latestSameValue=nums[i]
+			log.Printf("i: %+v, toDeleteCount: %+v, same value: %+v \n", i, toDeleteCount, nums[i])
 			continue
 		}
+		log.Println("totalDeletedCount", totalDeletedCount)
+		log.Println("toDeleteCount", toDeleteCount)
+		totalDeletedCount+=toDeleteCount
+		log.Println("totalDeletedCount", totalDeletedCount)
 		if toDeleteCount > 0 {
 			// 0 1 1 1 2
 			//   1     4
 			//     2 3
 			for j := i; j < len(nums); j++ {
-				iBefore := j-toDeleteCount
-				valueBefore := nums[iBefore]
-				nums[iBefore]=nums[j]
-				log.Printf("changing nums[%+v] from %+v to nums[%+v] %+v", iBefore, valueBefore, i, nums[i])
+
+				jBefore := j-toDeleteCount
+				log.Printf("i	      : %+v", i)
+				log.Printf("latestSameValue: '%+v'", latestSameValue)
+				log.Printf("toDeleteCount: %+v", toDeleteCount)
+				log.Printf("jBefore: '%+v'", jBefore)
+				log.Printf("j      : '%+v'", j)
+				valueBefore := nums[jBefore]
+				log.Printf("valueBefore: %+v", valueBefore)
+				log.Printf("valueAfter : %+v", nums[j])
+
+				log.Printf("nums before: %+v\n", strArr(nums))
+				nums[jBefore]=nums[j]
+				log.Printf("nums after : %+v\n\n\n", strArr(nums))
 			}
+			iBefore := i
+			i = i - toDeleteCount
+			log.Println("lastGoodIndex changing", lastGoodIndex)
+			lastGoodIndex = i
+			log.Println("lastGoodIndex changed ", lastGoodIndex)
+			log.Printf("changing i from %+v to %+v, toDeleteCount: %+v\n", iBefore, toDeleteCount, i)
+
 			toDeleteCount = 0
+			latestSameValue=0
 		}
+
 	}
-	return len(nums)-totalDeletedCount
+	log.Println("totalDeletedCount", totalDeletedCount)
+	log.Println("len(nums)", len(nums))
+	log.Println("lastGoodIndex", lastGoodIndex)
+	lastGoodLen := lastGoodIndex + 1
+	lastGoodLenPlusSkippedOne := lastGoodLen + 1
+	return lastGoodLenPlusSkippedOne
+}
+
+func strArr(nums []int) string {
+	var vv []string
+	for i, v := range nums {
+		vv = append(vv, fmt.Sprintf("%+v:%+v", i, v))
+	}
+	return fmt.Sprintf("%+v   %+v", nums, strings.Join(vv, ", "))
 }
--- remove-dupl-arr_test.go
+++ remove-dupl-arr_test.go
@@ -3,16 +3,24 @@ package leetcode
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
 func TestRemoveDuplicates(t *testing.T) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	type testCase struct {
 		inp     []int
 		expArr  []int
 		expLeft int
 	}
-	cases := []testCase{{inp: []int{1,1,2}, expArr: []int{1,2}, expLeft: 2}}
+	cases := []testCase{
+		{inp: []int{1,1,2}, expArr: []int{1,2}, expLeft: 2},
+		{inp: []int{0,0,1,1}, expArr: []int{0,1}, expLeft: 2},
+		{inp: []int{0,0,1,1,1}, expArr: []int{0,1}, expLeft: 2},
+		{inp: []int{0,0,1,1,1,2,2}, expArr: []int{0,1,2}, expLeft: 3},
+		{inp: []int{0,0,1,1,1,2,2,3,3,4}, expArr: []int{0,1,2,3,4}, expLeft: 5},
+	}
 	for i, c := range cases {
 		t.Run(fmt.Sprintf("%+v", i), func(t *testing.T) {
 			t.Logf("c.inp 1: %+v", c.inp)
